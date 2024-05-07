f832eefd88efdd06937d272d2da8b3ecafe9384e
Ava Tuntikanokporn <150287550+ava-tunti@users.noreply.github.com>
2024-05-07 14:02:06 -0700

    Update withdraw.js

--- withdraw.js
+++ withdraw.js
@@ -1,12 +1,100 @@
-function Withdraw(){
-  const ctx = React.useContext(UserContext);
+// function Withdraw(){
+//   const ctx = React.useContext(UserContext);
+//   return (
+//     <>
+//     <h1>Withdraw</h1>
+//     {JSON.stringify(ctx)}<br/>
+//     </>
+//   );
+// }
+
+function Withdraw() {
+  const [withdraw, setWithdraw] = React.useState("");
+  
+  function handleWithdraw = (amount) => {
+    if (parseFloat(amount) > balance) {
+      alert("You don't have enough balance to withdraw this amount");
+      return false;
+    }
+    
+    setBalance(balance - parseFloat(amount));
+    setTransactions([
+      ...transactions,
+      { type: "Withdraw", amount: amount, id: transactions.length + 1, timestamp: new Date()},
+    ]);
+    alert("Withdraw successful!");
+  };
+
+  function onWithdrawClick() {
+    if (withdraw === "" || withdraw <= 0) {
+      alert("Please enter positive value");
+      return;
+    }
+    handleWithdraw(withdraw);
+    setWithdraw("");
+  }
+
+  if (!authenticated) {
+    return <Navigate to="/log-in" />;
+  }
+
+  function onWithdrawChange(e) {
+    if (!validateNumber(e.target.value)) {
+      alert("Please enter a valid number");
+      return;
+    }
+    setWithdraw(e.target.value);
+  }
+
+  if (!authenticated) {
+    return <Navigate to="/log-in" />;
+  }
+
   return (
-    <>
-    <h1>Withdraw</h1>
-    {JSON.stringify(ctx)}<br/>
-    </>
+    <div className="container mt-5">
+      <HorizontalBalanceCard />
+      <div className="row flex-wrap align-items-center justify-content-center">
+        <div className="col-sm-12 col-md-6 d-flex flex-column justify-content-center align-items-center">
+          <Card
+            bgcolor="danger"
+            header="WITHDRAW"
+            status=""
+            text="Withdraw money from your account"
+            body={
+              <>
+                <div className="mb-3">
+                  <label htmlFor="deposit" className="form-label">
+                    Amount
+                  </label>
+
+                  <input
+                    type="number"
+                    className="form-control"
+                    id="deposit"
+                    placeholder="Enter deposit amount"
+                    value={withdraw}
+                    onChange={onWithdrawChange}
+                  />
+                </div>
+
+                <button
+                  type="submit"
+                  className="btn btn-light"
+                  onClick={onWithdrawClick}
+                  disabled={withdraw === ""}
+                >
+                  Withdraw
+                </button>
+              </>
+            }
+          />
+        </div>
+      </div>
+    </div>
   );
 }
+
+
 
 // function Withdraw() {
 //   const [withdrawAmount, setWithdrawAmount] = useState('');
